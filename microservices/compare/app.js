d24f121d323f92c9b777f62133f93e20cdf19a46
jaybengco <jay.bengco@tooltwist.com>
2016-08-18 11:58:29 +0800

    TEST nginx

--- microservices/compare/app.js
+++ microservices/compare/app.js
@@ -4,12 +4,16 @@ var express = require('express');
 var port = process.env.PORT || '3000';
 var app = express();
 
-app.post('/compare/users', function (req, res, next) {
+app.post('/api/compare/users', function (req, res, next) {
     return res.json([{ id: '1', name: 'johnDoe' }, { id: '2', name: 'janeDoe' }]);
 });
 
-app.post('/compare/addresses', function (req, res, next) {
+app.post('/api/compare/addresses', function (req, res, next) {
     return res.json([{ "street": "test123" }, { "street": "test123" }]);
+});
+
+app.post('/api/search/compare/addresses', function (req, res, next) {
+    return res.json([{ "street": "1test123" }, { "street": "1test123" }]);
 });
 
 var server = http.createServer(app);
